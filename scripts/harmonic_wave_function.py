1e77997085b1007e462354ac979b82f74f540041
bchardim <bchardim@redhat.com>
2018-03-04 18:05:42 +0100

    Using true division by default

--- scripts/harmonic_wave_function.py
+++ scripts/harmonic_wave_function.py
@@ -1,8 +1,15 @@
 #!/usr/bin/python -Wignore
 
+# Example 'harmonic_wave_function.py 5'
+#
+# The location (x) of a particle represented by the wave function psi (y) is more
+# accurately known for higher n. Conversely, the momentum is better known for 
+# lower n since there are fewer wavelengths (hence, fewer energies and momenta) 
+# contributing to psi if nis smaller -> [position-momentum uncertainty principle]
+#
 
-# Example 'harmonic_wave_function.py 5' 
-
+# Force true division
+from __future__ import division
 import sys
 import sympy
 import unicodedata
@@ -13,9 +20,9 @@ from matplotlib import pyplot as plt
 def harmonic(x,n):
     func = 0
     z = x * pi
-    amp = np.true_divide(2,(n + 1))
+    amp = 2/(n + 1)
     for i in xrange(1,n+1,2) :
-        sign = (-1)**np.true_divide((i-1),2)
+        sign = (-1)**((i-1)/2)
         func = func + amp*sign*np.sin(i * z)
     return func
 
@@ -23,9 +30,10 @@ def harmonic(x,n):
 
 if __name__ == "__main__":
 
-    x = np.linspace(0,1,500) # pi multiples [0,1]*pi
+    # Generate plot 
+    x = np.linspace(0,1,500) # pi fractions [0,1]*pi
     plt.plot(x, harmonic(x,int(sys.argv[1])))
-    plt.xlabel(unicodedata.lookup("GREEK SMALL LETTER PI"))
+    plt.xlabel("x " +  unicodedata.lookup("GREEK SMALL LETTER PI"))
     plt.ylabel(unicodedata.lookup("GREEK SMALL LETTER PSI"),rotation=0)
     plt.axhline(y=0, color='grey', linestyle='dashed')
     plt.show()
